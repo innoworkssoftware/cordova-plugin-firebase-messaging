5c6c3d1d3bb7e765c15e46754ef0dd4132bb0490
Sai Sree Harsha Dubagunta <dharsha@innoworkssoftware.com>
2020-08-06 11:28:36 +0530

    Missed import statements

--- src/android/FirebaseMessagingPluginService.java
+++ src/android/FirebaseMessagingPluginService.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
@@ -16,6 +17,7 @@ import androidx.core.app.NotificationCompat;
 import androidx.core.content.ContextCompat;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import com.ebiz.mobile.skiptheline.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
